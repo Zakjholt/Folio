bba86544933cfb96b308bcc03de11517b1c54004
Zak <zak.j.holt@gmail.com>
2019-04-29 17:14:20 -0400

    get fade transition working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <folio></folio>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Folio from "./components/Folio";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Folio
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Comfortaa");
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Comfortaa", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Folio.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <h4>Zak</h4>
+
+    <div class="blurb">
+      <transition
+        name="custom-classes-transition"
+        mode="out-in"
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+      >
+        <span :key="currentTech" :class="currentTech">{{currentTech}}</span>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  padding: 48px;
+
+  color: #212122;
+}
+
+h4 {
+  font-size: 16px;
+}
+
+.blurb {
+  font-size: 10px;
+}
+
+span {
+  font-size: 24px;
+}
+
+.blurb h4 {
+  font-weight: 900;
+}
+</style>
+
+<script>
+export default {
+  mounted() {
+    const self = this;
+    setInterval(() => {
+      self.incrementTech();
+    }, 1000);
+  },
+
+  data() {
+    return {
+      techs: ["react", "vue", "elm", "graphql", "go", "elixir", "nodejs"],
+      index: 0
+    };
+  },
+
+  methods: {
+    incrementTech() {
+      this.index === this.techs.length - 1 ? (this.index = 0) : this.index++;
+    }
+  },
+
+  computed: {
+    currentTech: function() {
+      return this.techs[this.index];
+    }
+  }
+};
+</script>
+
+
