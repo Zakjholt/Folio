ac2d2a7b93f44460bb53e030e04818c2ee56f9eb
Zak <zak.j.holt@gmail.com>
2019-05-01 16:21:59 -0400

    add contact details

--- src/components/Folio.vue
+++ src/components/Folio.vue
@@ -2,27 +2,63 @@
   <div class="container">
     <h4>Zak</h4>
 
-    <div class="blurb">
-      <transition
-        name="custom-classes-transition"
-        mode="out-in"
-        enter-active-class="animated fadeIn"
-        leave-active-class="animated fadeOut"
-      >
-        <strong :key="currentTech" :class="currentTech">{{currentTech}}</strong>
-      </transition>
-    </div>
-    <div class="contact">contact</div>
+    <transition-group
+      mode="out-in"
+      enter-active-class="animated fadeInRightBig"
+      leave-active-class="animated fadeOutLeftBig"
+    >
+      <ul class="contact-details" key="contact" v-if="showContact">
+        <li>
+          <a href="mailto:zak.j.holt@gmail.com">zak.j.holt@gmail.com</a>
+        </li>
+        <li>
+          <a href="http://github.com/zakjholt">github.com/zakjholt</a>
+        </li>
+      </ul>
+      <div class="blurb" key="techs" v-else>
+        <transition
+          mode="out-in"
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
+        >
+          <strong :key="currentTech" :class="currentTech">{{currentTech}}</strong>
+        </transition>
+      </div>
+    </transition-group>
+    <div class="contact" @click="toggleShowContact">{{actionText}}</div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .container {
   padding: 48px;
+  position: relative;
+  height: 100vh;
 }
 
-.blurb {
-  align-self: center;
+.blurb,
+.contact-details {
+  position: absolute;
+  top: 20vh;
+  left: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.contact-details {
+  list-style: none;
+  padding-left: 0;
+
+  li {
+    margin-top: 16px;
+  }
+
+  a {
+    color: #232322;
+  }
 }
 
 h4 {
@@ -73,12 +109,17 @@ export default {
 
   data() {
     return {
+      showContact: false,
       techs: ["react", "vue", "elm", "graphql", "go", "elixir", "nodejs"],
       index: 0
     };
   },
 
   methods: {
+    toggleShowContact() {
+      this.showContact = !this.showContact;
+    },
+
     incrementTech() {
       this.index === this.techs.length - 1 ? (this.index = 0) : this.index++;
     }
@@ -87,6 +128,10 @@ export default {
   computed: {
     currentTech: function() {
       return this.techs[this.index];
+    },
+
+    actionText: function() {
+      return this.showContact ? "back" : "contact";
     }
   }
 };
